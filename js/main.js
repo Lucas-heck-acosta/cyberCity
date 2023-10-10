7d282bffe6cb7f02c404bc3b1da9ebc036671a9a
Lucas Acosta <lucas.heck@live.com>
2023-10-09 22:09:13 -0300

    update layout

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    
+    setTimeout(function() {
+        $('#modal').modal('show')
+    }, 10000)
+
+
+    $('#dropdownMenuButton').click(function() {
+        $(this).toggleClass('rotate-down');
+    });
+
+    $('#dropdownMenu').on('hidden.bs.dropdown', function() {
+        $('#dropdownMenuButton').removeClass('rotate-down');
+    });
+
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        }
+    })
+
+    $('#confirmar').click(function(e) {
+        if ($('form').valid())
+        {
+            $('#modal').modal('hide');
+        } 
+        else
+        {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+    });
+});
+
